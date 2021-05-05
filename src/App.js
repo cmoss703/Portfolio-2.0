ad24e755c4eb2c3a486e40a48694197354f81669
Christina <christinalmoss@gmail.com>
2021-05-05 00:05:27 -0500

    tryinf to fix deploy

--- src/App.js
+++ src/App.js
@@ -19,9 +19,6 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route exact path="/portfolio-2">
-            <Home />
-          </Route>
           <Route exact path="/portfolio">
             <Portfolio />
           </Route>
